3fd5344680c9248c44c0664eabc6b838acbb5f33
Julian Geiger <julian.geiger@psi.ch>
2024-05-01 07:41:31 +0200

    Added `get_builder_from_ase` to `helpers.py`

--- aiida_koopmans/helpers.py
+++ aiida_koopmans/helpers.py
@@ -97,3 +97,70 @@ def get_code(entry_point, computer):
     )
     code.label = executable
     return code.store()
+
+def get_builder_from_ase(pw_calculator):
+    from aiida import load_profile, orm
+    from aiida_quantumespresso.common.types import ElectronicType
+    from aiida_quantumespresso.workflows.pw.base import PwBaseWorkChain, PwCalculation
+
+    load_profile()
+
+    """
+    We should check automatically on the accepted keywords in PwCalculation and where are. Should be possible.
+    we suppose that the calculator has an attribute called mode e.g.
+    
+    pw_calculator.mode = {
+        "pw_code": "pw-7.2-ok@localhost",
+        "metadata": {
+        "options": {
+            "max_wallclock_seconds": 3600,
+            "resources": {
+                "num_machines": 1,
+                "num_mpiprocs_per_machine": 1,
+                "num_cores_per_mpiproc": 1
+            },
+            "custom_scheduler_commands": "export OMP_NUM_THREADS=1"
+        }
+    }
+    }
+    """
+    aiida_inputs = pw_calculator.mode
+    calc_params = pw_calculator._parameters
+    structure = orm.StructureData(ase=pw_calculator.atoms)
+
+    pw_overrides = {
+        "CONTROL": {},
+        "SYSTEM": {"nosym": True, "noinv": True},
+        "ELECTRONS": {},
+    }
+
+    for k in ["calculation", "verbosity"]:  # ,"prefix"
+        if k in calc_params.keys():
+            pw_overrides["CONTROL"][k] = calc_params[k]
+
+    for k in ["tot_charge", "tot_magnetization", "nbnd", "ecutwfc", "ecutrho", "nspin"]:
+        if k in calc_params.keys():
+            pw_overrides["SYSTEM"][k] = calc_params[k]
+
+    for k in ["conv_thr"]:
+        if k in calc_params.keys():
+            pw_overrides["ELECTRONS"][k] = calc_params[k]
+
+    builder = PwBaseWorkChain.get_builder_from_protocol(
+        code=aiida_inputs["pw_code"],
+        structure=structure,
+        overrides={
+            "pseudo_family": "PseudoDojo/0.4/PBE/FR/standard/upf",
+            "pw": {"parameters": pw_overrides},
+        },
+        electronic_type=ElectronicType.INSULATOR,
+    )
+    builder.pw.metadata = aiida_inputs["metadata"]
+
+    builder.kpoints = orm.KpointsData()
+    builder.kpoints.set_kpoints_mesh(calc_params["kpts"])
+
+    if hasattr(pw_calculator, "parent_folder"):
+        builder.pw.parent_folder = pw_calculator.parent_folder
+
+    return builder
