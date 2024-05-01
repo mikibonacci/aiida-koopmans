3115ebcb29b2e8897e0efa12ad81c0405107c5a2
Julian Geiger <julian.geiger@psi.ch>
2024-05-01 07:47:33 +0200

    `from_wann2kc_to_KcwCalculation` to helpers

--- aiida_koopmans/helpers.py
+++ aiida_koopmans/helpers.py
@@ -164,3 +164,86 @@ def get_builder_from_ase(pw_calculator):
         builder.pw.parent_folder = pw_calculator.parent_folder
 
     return builder
+
+# MB mod:
+def from_wann2kc_to_KcwCalculation(wann2kc_calculator):
+    """
+    The input parent folder is meant to be set later, at least for now.
+    """
+
+    from aiida_koopmans.calculations.kcw import KcwCalculation
+    from aiida import orm, load_profile
+
+    load_profile()
+
+    builder = KcwCalculation.get_builder()
+
+    control_namelist = [
+        "kcw_iverbosity",
+        "kcw_at_ks",
+        "lrpa",
+        "mp1",
+        "mp2",
+        "mp3",
+        "homo_only",
+        "read_unitary_matrix",
+        "l_vcut",
+        "spin_component",
+    ]
+
+    control_dict = {
+        k: v if k in control_namelist else None
+        for k, v in wann2kc_calculator.parameters.items()
+    }
+    control_dict["calculation"] = "wann2kcw"
+
+    if not any(wann2kc_calculator.atoms.pbc):
+        control_dict["assume_isolated"] = "m-t"
+
+    for k in list(control_dict):
+        if control_dict[k] is None:
+            control_dict.pop(k)
+
+    wannier_namelist = [
+        "check_ks",
+        "num_wann_occ",
+        "num_wann_emp",
+        "have_empty",
+        "has_disentangle",
+    ]
+
+    wannier_dict = {
+        k: v if k in wannier_namelist else None
+        for k, v in wann2kc_calculator.parameters.items()
+    }
+
+    for k in list(wannier_dict):
+        if wannier_dict[k] is None:
+            wannier_dict.pop(k)
+
+    wann2kcw_params = {
+        "CONTROL": control_dict,
+        "WANNIER": wannier_dict,
+    }
+
+    builder.parameters = orm.Dict(wann2kcw_params)
+    builder.code = orm.load_code(wann2kc_calculator.mode["kcw_code"])
+    builder.metadata = wann2kc_calculator.mode["metadata"]
+    if "metadata_kcw" in wann2kc_calculator.mode:
+        builder.metadata = wann2kc_calculator.mode["metadata_kcw"]
+    builder.parent_folder = wann2kc_calculator.parent_folder
+
+    if hasattr(wann2kc_calculator, "wannier90_files"):
+        builder.wann_u_mat = wann2kc_calculator.wannier90_files["occ"]["u_mat"]
+        builder.wann_emp_u_mat = wann2kc_calculator.wannier90_files["emp"]["u_mat"]
+        builder.wann_emp_u_dis_mat = wann2kc_calculator.wannier90_files["emp"][
+            "u_dis_mat"
+        ]
+        builder.wann_centres_xyz = wann2kc_calculator.wannier90_files["occ"][
+            "centres_xyz"
+        ]
+        builder.wann_emp_centres_xyz = wann2kc_calculator.wannier90_files["emp"][
+            "centres_xyz"
+        ]
+
+    return builder
